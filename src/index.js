14aa8d27b539a58627279698ff36d12bcee61540
Fuzi18 <fuzesi.t2003@gmail.com>
2022-11-15 10:09:53 +0100

    s

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './style.css';
 
+document.addEventListener("DOMContentLoaded", ()=>{
+    let resoult;
+        async function LoadData(){
+            let response = await fetch('/quotes.json');
+            resoult = await response.json();
+        }
+    
+        LoadData();
+    
+        function printData(quotes){
+            document.getElementById('quoteList').textContent = "";
+    
+            for (let q of quotes){
+                let li = document.createElement('li');
+                li.innerHTML = q.quote + " " + "<br>" + q.author + "<hr>";
+                document.getElementById('quoteList').appendChild(li);
+            }
+            
+        }
+    
+    
+       
+              });
+    
+  
+
